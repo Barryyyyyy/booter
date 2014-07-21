b5f5976aa350e4855f33468be2490f492e439020
Rhondal <llabenski@comcast.net>
2014-07-21 12:34:01 -0400

    First Commit
    First Commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+include(ROOT . 'application/libs' . DS . 'confighandler.php');
+
+/* Note: Make sure to create an empty database, the installer will do the rest */
+Config::Write('HOST', 'localhost');
+Config::Write('USER', 'root');
+Config::Write('PASS', 'Nutley22');
+Config::Write('DBNAME', 'stress');
+
+Config::Write('SITENAME', 'zk Stresser');
+Config::Write('DENYMSG', 'Kindly fuck off or you will be swatted');
+Config::Write('BASEURL', $_SERVER['SERVER_NAME']. '/Api'); //make sure to add your directory here, if you are in a directory!
+Config::Write('MAIN_ACCOUNT', 'Sh1eld');
+?>
